391b8daa2de01448bbaff7bfc9345dd61908ff21
Kyle Petryszak <6314611+ProjectInitiative@users.noreply.github.com>
2024-09-19 20:29:49 -0500

    adding initial nix configs to support building in the nix env

---
+++ flake.nix
@@ -0,0 +1,99 @@
+{
+  description = "LSP-AI: An open-source language server for AI-powered functionality";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, rust-overlay }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs { 
+          inherit system overlays; 
+          config.allowUnfree = true;
+        };
+        rustVersion = pkgs.rust-bin.stable.latest.default;
+        
+        commonEnv = {
+          OPENSSL_NO_VENDOR = 1;
+          OPENSSL_DIR = "${pkgs.openssl.dev}";
+          OPENSSL_LIB_DIR = "${pkgs.openssl.out}/lib";
+          RUSTFLAGS = "-C target-cpu=native";
+        };
+
+        commonBuildInputs = with pkgs; [ 
+          openssl 
+          openssl.dev
+          zlib
+        ];
+
+        commonNativeBuildInputs = with pkgs; [ 
+          pkg-config 
+          cmake 
+          rustVersion 
+          perl
+        ];
+
+      in rec {
+        packages = rec {
+          default = pkgs.rustPlatform.buildRustPackage ({
+            pname = "lsp-ai";
+            version = "0.7.0";
+
+            src = ./.;
+
+            cargoLock = {
+              lockFile = ./Cargo.lock;
+              allowBuiltinFetchGit = true;
+            };
+
+            nativeBuildInputs = commonNativeBuildInputs;
+            buildInputs = commonBuildInputs;
+
+            buildFeatures = [ "all" ];
+
+            doCheck = false;
+
+            meta = with pkgs.lib; {
+              description = "An open-source language server that serves as a backend for AI-powered functionality";
+              homepage = "https://github.com/SilasMarvin/lsp-ai";
+              license = licenses.mit;
+              maintainers = [ /* Add maintainers here */ ];
+            };
+          } // commonEnv);
+
+          devPackage = default.overrideAttrs (oldAttrs: {
+            name = "lsp-ai-dev";
+            
+            buildPhase = ''
+              export CARGO_HOME="/tmp/.cargo"
+              export RUSTUP_HOME="/tmp/.rustup"
+              cargo build --features all
+            '';
+
+            installPhase = ''
+              mkdir -p $out/bin
+              cp target/debug/lsp-ai $out/bin/
+            '';
+          });
+        };
+
+        devShells.default = pkgs.mkShell ({
+          inputsFrom = [ packages.default ];
+          packages = with pkgs; [
+            rust-analyzer
+            clippy
+            rustfmt
+          ];
+          
+          shellHook = ''
+            export CARGO_HOME="/tmp/.cargo"
+            export RUSTUP_HOME="/tmp/.rustup"
+          '';
+        } // commonEnv);
+      }
+    );
+}
